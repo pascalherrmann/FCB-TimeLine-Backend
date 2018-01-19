e1b4b3dacfb7bd1e6c212357de4894b764d4559d
Pascal Herrmann <pasher1@web.de>
2018-01-19 20:48:45 +0100

    added reaction socket

--- server.js
+++ server.js
@@ -1,51 +1,61 @@
-var app = require('express')();
+var express = require('express');
+const util = require('util')
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+/*
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/public/index.html');
 });
+*/
 
-io.on('connection', function(socket){
-  console.log('a user connected');
+app.use(express.static(__dirname + '/public'));
+
+/*
+Sample Code
+*/
+
+app.get('/test', function (req, res) {
+    console.log("test");
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
-    
-
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
-});
-    
-
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
-  });
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });
 
-
-
-
-
-
-io.emit('some event', { for: 'everyone' });
-
-// send message to everyone except 
-io.on('connection', function(socket){
-  socket.broadcast.emit('hi');
+io.on('connection', function (socket) {
+    console.log('a user connected');
+    //todo: load last reactions
+    socket.on('disconnect', function () {
+        console.log('user disconnected');
+    });
 });
 
 
+io.on('connection', function (socket) {
+    socket.on('chat message', function (msg) {
+        console.log('message: ' + msg);
+        console.log(util.inspect(msg, false, null))
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+        io.emit('chat message', msg);
+    });
+});
+
+
+/*
+MAIN
+*/
+
+//
+// SENDING REACTION
+//
+// get a reaction and share it with everyone 
+io.on('connection', function (socket) {
+    socket.on('reaction', function (reaction) {
+        
+        //todo: save to DB
+        console.log(util.inspect(reaction, false, null))
+        io.emit('reaction', reaction);
+    });
 });
